0be41334080c4d12a0de2bd86045c734c8f92323
MWangxj <qiupeng1993@gmail.com>
2017-10-20 17:33:25 +0800

    rm the wrong method of GetHmacStr

--- models/hmacsha1/hmacsha1.go
+++ models/hmacsha1/hmacsha1.go
@@ -5,14 +5,18 @@ import (
 	"crypto/sha1"
 	"errors"
 	"encoding/base64"
+	"fmt"
 )
 
 func GetHmacStr(paramstr *string, keystr *string) (hmacstr *string, err error) {
 	if paramstr == nil || keystr == nil {
 		return nil, errors.New("GetHmacStr parameter pointer shouldn't be nil")
 	}
-	hm := hmac.New(sha1.New, []byte(*keystr))
-	hm.Write([]byte(*paramstr))
-	temp := base64.StdEncoding.EncodeToString(hm.Sum(nil))
+	bytestr:=[]byte(*keystr+"&")
+	fmt.Println(bytestr)
+	hmacsha1 := hmac.New(sha1.New, []byte(*keystr+"&"))
+	hmacsha1.Write([]byte(*paramstr))
+	fmt.Println(hmacsha1.Sum(nil))
+	temp := base64.StdEncoding.EncodeToString(hmacsha1.Sum(nil))
 	return &temp, nil
 }
--- test.go
+++ test.go
@@ -18,28 +18,28 @@ func main() {
 		fmt.Print(*strr)
 	}*/
 
-	httpurl:="http://ecs.aliyuncs.com/?TimeStamp=2016-02-23T12:46:24Z&Format=XML&AccessKeyId=testid&Action=DescribeRegions&SignatureMethod=HMAC-SHA1&SignatureNonce=3ee8c1b8-83d3-44af-a94f-4e0ad82fd6cf&Version=2014-05-26&SignatureVersion=1.0"
-	var httpurlp *string
-	httpurlp=&httpurl
-	httpmethod:="GET"
-	var httpmethodp *string
-	httpmethodp=&httpmethod
-	signstr,err:=signature.SignatureString(httpurlp,httpmethodp)
-	if err==nil {
-		fmt.Println(*signstr)
-	}
+	//httpurl:="http://ecs.aliyuncs.com/?TimeStamp=2016-02-23T12:46:24Z&Format=XML&AccessKeyId=testid&Action=DescribeRegions&SignatureMethod=HMAC-SHA1&SignatureNonce=3ee8c1b8-83d3-44af-a94f-4e0ad82fd6cf&Version=2014-05-26&SignatureVersion=1.0"
+	//var httpurlp *string
+	//httpurlp=&httpurl
+	//httpmethod:="GET"
+	//var httpmethodp *string
+	//httpmethodp=&httpmethod
+	//signstr,err:=signature.SignatureString(httpurlp,httpmethodp)
+	//if err==nil {
+	//	fmt.Println(*signstr)
+	//}
 
 	str:="GET&%2F&AccessKeyId%3Dtestid&Action%3DDescribeRegions&Format%3DXML&SignatureMethod%3DHMAC-SHA1&SignatureNonce%3D3ee8c1b8-83d3-44af-a94f-4e0ad82fd6cf&SignatureVersion%3D1.0&TimeStamp%3D2016-02-23T12%253A46%253A24Z&Version%3D2014-05-26"
 	fmt.Println(str)
 	var strp *string
 	strp=&str
-	keys:="testsecret&"
+	keys:="testsecret"
 	var keysp *string
 	keysp=&keys
-	signstrr,err:=signature.GetSignature(signstr,keysp)
-	strpr,_:=signature.GetSignature(strp,keysp)
+	//signstrr,err:=signature.GetSignature(signstr,keysp)
+	strpr,err:=signature.GetSignature(strp,keysp)
 	if err==nil {
-		fmt.Println(*signstrr)
+		//fmt.Println(*signstrr)
 		fmt.Println(*strpr)
 	}
 
